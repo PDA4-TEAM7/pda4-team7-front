362d819ef680893fc15e5b6e8cc8f2b64e9c70e7
soyeon <uyyu1734@gmail.com>
2024-06-06 20:59:46 +0900

    feat: 우리 서버랑 통신할 baseAPI

---
+++ src/apis/baseAPI.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+const { VITE_BASE_URL } = import.meta.env;
+
+//우리 서버랑 통신할 Api 세팅
+export class BaseApi {
+  fetcher;
+  constructor() {
+    //미리생성
+    axios.defaults.withCredentials = true;
+    this.fetcher = axios.create({
+      baseURL: VITE_BASE_URL,
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+  }
+}
