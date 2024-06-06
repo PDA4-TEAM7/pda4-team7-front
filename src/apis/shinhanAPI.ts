2fa893eb867c9bf4a18f9a9e62aed96c9cc08973
soyeon <uyyu1734@gmail.com>
2024-06-06 21:11:39 +0900

    feat: 신한 API 조회

--- src/apis/shinhanAPI.ts
+++ src/apis/shinhanAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const { VITE_SHIN_HAN_URL } = import.meta.env;
 
-interface IStockData {
+export interface IStockData {
   incpPrc: number;
   incpYmd: Date;
   reveRt: number;
@@ -30,17 +30,18 @@ export class ShinhanApi {
       baseURL: VITE_SHIN_HAN_URL,
       headers: {
         'Content-type': 'application/json',
+        apiKey: 'apiKey-value',
       },
     });
   }
   //GET: 신한 추천포트폴리오
-  async getRecommendPortfolio(): Promise<{ list: IStockData[] }> {
+  async getRecommendPortfolio(): Promise<IStockData[]> {
     try {
       const resp: IShinhanRecommendPortfolioRes = await this.fetcher.get('recommend/portfolio').then((res) => res.data);
-      return resp.dataBody;
+      return resp.dataBody.list;
     } catch (e) {
       console.log('error getRecommendPortfolio :', e);
-      return { list: [] };
+      return [];
     }
   }
 }
--- src/routes/dev/page.tsx
+++ src/routes/dev/page.tsx
@@ -1,4 +1,17 @@
+import { ShinhanApi, IStockData } from '@/apis/shinhanAPI';
+import useSWR from 'swr';
+
 //테스트용 페이지
 export default function Dev() {
-  return <div>page</div>;
+  const service = new ShinhanApi();
+  const { data, isLoading } = useSWR<IStockData[]>(`shinhanRecommend`, () => service.getRecommendPortfolio());
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      page
+      <div>{data && data.map((item) => <div>{item.stbdName}</div>)}</div>
+    </div>
+  );
 }
