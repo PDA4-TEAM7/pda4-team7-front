d82d35c8b577f203ff32d4cdeab4233be62f2741
soyeon <uyyu1734@gmail.com>
2024-06-06 21:16:55 +0900

    feat: 공용모달 추가. 모달예제는 dev 페이지에 있음.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider } from 'react-router-dom';
 import MainRouter from './routes/main-router';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import CommonPopup from './components/CommonPopup';
 
 function App() {
   return (
     <Provider store={store}>
       <RouterProvider router={MainRouter}></RouterProvider>
+      <CommonPopup />
     </Provider>
   );
 }
--- src/routes/dev/page.tsx
+++ src/routes/dev/page.tsx
@@ -1,16 +1,24 @@
 import { ShinhanApi, IStockData } from '@/apis/shinhanAPI';
 import useSWR from 'swr';
+import useModal from '@/hooks/useModal';
 
 //테스트용 페이지
 export default function Dev() {
   const service = new ShinhanApi();
   const { data, isLoading } = useSWR<IStockData[]>(`shinhanRecommend`, () => service.getRecommendPortfolio());
+  const { open, close } = useModal();
+  const handleModal = () => {
+    open('title', '이런식으로 모달을 사용하면 됩니다.', close);
+  };
   if (isLoading) {
     return <div>Loading...</div>;
   }
   return (
     <div>
-      page
+      <p>test page</p>
+      <button className="btn border m-2 p-1" onClick={handleModal}>
+        모달 버튼
+      </button>
       <div>{data && data.map((item) => <div>{item.stbdName}</div>)}</div>
     </div>
   );
