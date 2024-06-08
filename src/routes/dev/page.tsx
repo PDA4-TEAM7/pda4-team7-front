3974e348ae65b6ae31effa9f58a593c129ba09ec
soyeon <uyyu1734@gmail.com>
2024-06-08 20:05:02 +0900

    feat: chart.js 예제 코드

--- src/routes/dev/page.tsx
+++ src/routes/dev/page.tsx
@@ -1,7 +1,9 @@
-import { ShinhanApi, IStockData } from '@/apis/shinhanAPI';
-import useSWR from 'swr';
-import useModal from '@/hooks/useModal';
-import IconHeart from '@/assets/icon-heart.svg?react';
+import { ShinhanApi, IStockData } from "@/apis/shinhanAPI";
+import useSWR from "swr";
+import useModal from "@/hooks/useModal";
+import IconHeart from "@/assets/icon-heart.svg?react";
+import Chart from "./components/Chart";
+import LineChart from "./components/LineChart";
 
 //테스트용 페이지
 export default function Dev() {
@@ -9,7 +11,7 @@ export default function Dev() {
   const { data, isLoading } = useSWR<IStockData[]>(`shinhanRecommend`, () => service.getRecommendPortfolio());
   const { open, close } = useModal();
   const handleModal = () => {
-    open('title', '이런식으로 모달을 사용하면 됩니다.', close);
+    open("title", "이런식으로 모달을 사용하면 됩니다.", close);
   };
   if (isLoading) {
     return <div>Loading...</div>;
@@ -19,12 +21,19 @@ export default function Dev() {
       <p>test page</p>
       <p>
         <span> svg 넣기 :</span>
-        <IconHeart width={100} height={100} fill={'#2cffd1'} stroke={'#2cffd1'} />
+        <IconHeart width={100} height={100} fill={"#2cffd1"} stroke={"#2cffd1"} />
       </p>
       <button className="btn border m-2 p-1" onClick={handleModal}>
         모달 버튼
       </button>
       <div>{data && data.map((item) => <div>{item.stbdName}</div>)}</div>
+      <div>
+        <p>chart example </p>
+        <p>Pie chart</p>
+        <Chart />
+        <p>Line chart</p>
+        <LineChart />
+      </div>
     </div>
   );
 }
