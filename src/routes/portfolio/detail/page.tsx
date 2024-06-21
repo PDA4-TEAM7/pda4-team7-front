675edbc0b29c97009a7804ff268b67e9cefe3bdd
soyeon <uyyu1734@gmail.com>
2024-06-21 10:25:59 +0900

    feat: 날짜 선택 모듈 mui 로 추가

--- src/routes/portfolio/detail/page.tsx
+++ src/routes/portfolio/detail/page.tsx
@@ -1,8 +1,12 @@
 import { useParams } from "react-router-dom";
-import DatePicker from "./DatePicker";
-
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useState } from "react";
+import { Dayjs } from "dayjs";
+import { TextField } from "@mui/material";
 // portfolio/detail/1
 export default function DetailPage() {
+  const [startDate, setStartDate] = useState<Dayjs | null>(null);
+  const [endDate, setEndDate] = useState<Dayjs | null>(null);
   const { id } = useParams();
   const title = "무슨 포폴이냐면요";
   return (
@@ -19,8 +23,27 @@ export default function DetailPage() {
       </nav>
       <div className="wrap-section flex flex-row">
         <div className="section flex-1">
-          <DatePicker />
-          section1
+          <div className="date-wrap flex">
+            <DatePicker
+              label={"시작"}
+              views={["month", "year"]}
+              value={startDate}
+              onChange={(newValue) => {
+                setStartDate(newValue);
+              }}
+            />
+            <DatePicker
+              label={"종료"}
+              views={["month", "year"]}
+              value={endDate}
+              onChange={(newValue) => {
+                setEndDate(newValue);
+              }}
+            />
+          </div>
+          {startDate && `${startDate.year()} ${startDate.month()}`}
+          <br />
+          {endDate && `${endDate.year()} ${endDate.month()}`}
         </div>
         <div className="section flex-1"></div>
       </div>
