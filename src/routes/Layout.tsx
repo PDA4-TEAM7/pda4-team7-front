28768cca1ba75876420d25c57dea69e8b9ea00b7
soyeon <uyyu1734@gmail.com>
2024-06-24 23:47:50 +0900

    feat:  비로그인시 마이페이지&내 포트폴리오 안보이게 하기

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -11,7 +11,6 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Button from "@mui/material/Button";
-
 import IconPortfolio from "@/assets/icon-portfolio.svg?react";
 import IconMy from "@/assets/icon-my.svg?react";
 import IconSub from "@/assets/icon-sub.svg?react";
@@ -23,21 +22,25 @@ const navMenu = [
     text: "포트폴리오",
     page: "/portfolio/mainPortfolio",
     icon: <IconPortfolio width={24} height={24} />,
+    display: "always",
   },
   {
     text: "구독 포트폴리오",
     page: "/portfolio/subscribe",
     icon: <IconSub width={24} height={24} />,
+    display: "always", //UI에는 띄우고 선택시 로그인 요청
   },
   {
     text: "마이 페이지",
     page: "/mypage",
     icon: <IconMy width={24} height={24} />,
+    display: "login", //로그인시 노출
   },
   {
     text: "나의 포트폴리오",
     page: "/portfolio/myportfolio",
     icon: <IconPortfolio width={24} height={24} />,
+    display: "login", //로그인시 노출
   },
 ];
 const openedMixin = (theme: Theme): CSSObject => ({
@@ -100,7 +103,6 @@ export default function Layout() {
 
   const handleNavigate = (page: string) => {
     navigate(page);
-    console.log("loca:", location.pathname);
   };
 
   const handleLogout = async () => {
@@ -155,66 +157,92 @@ export default function Layout() {
           </div>
         )}
         <List>
-          {navMenu.map(({ text, page, icon }, index) => (
-            <ListItem onClick={() => handleNavigate(page)} key={text + index} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? "initial" : "center",
-                  px: 2.5,
-                }}
-                className={`${location.pathname === page && "!bg-[#121417]"}`}
+          {navMenu.map(({ text, page, icon, display }, index) => {
+            if (display === "login" && !user.userId) return <></>;
+            return (
+              <ListItem
+                onClick={() => handleNavigate(page)}
+                key={text + index}
+                disablePadding
+                sx={{ display: "block" }}
               >
-                <ListItemIcon
+                <ListItemButton
                   sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : "auto",
-                    justifyContent: "center",
+                    minHeight: 48,
+                    justifyContent: open ? "initial" : "center",
+                    px: 2.5,
                   }}
-                  className="center !bg-transparent"
+                  className={`${location.pathname === page && "!bg-[#121417]"}`}
                 >
-                  {icon}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={{ opacity: open ? 1 : 0 }}
-                  className="text-[#E0E4EA] !bg-transparent"
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center",
+                    }}
+                    className="center !bg-transparent"
+                  >
+                    {icon}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={text}
+                    sx={{ opacity: open ? 1 : 0 }}
+                    className="text-[#E0E4EA] !bg-transparent"
+                  />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
         </List>
-        <div className="mt-auto mb-4 px-2">
-          {user.userId ? (
-            <Button
-              variant="text"
-              onClick={handleLogout}
-              fullWidth
-              sx={{
-                color: "#E0E4EA",
-                "&:hover": {
-                  backgroundColor: "transparent",
-                },
-              }}
-            >
-              로그아웃
-            </Button>
-          ) : (
-            <Button
-              variant="text"
-              onClick={handleLogin}
-              fullWidth
-              sx={{
-                color: "#E0E4EA",
-                "&:hover": {
-                  backgroundColor: "transparent",
-                },
-              }}
-            >
-              로그인
-            </Button>
-          )}
-        </div>
+        {open && (
+          <div className="mt-auto mb-4 px-2 text-right">
+            {user.userId ? (
+              <Button
+                variant="text"
+                onClick={handleLogout}
+                fullWidth
+                sx={{
+                  color: "#E0E4EA",
+                  "&:hover": {
+                    backgroundColor: "transparent",
+                  },
+                }}
+              >
+                로그아웃
+              </Button>
+            ) : (
+              <div className="flex justify-center items-center">
+                <Button
+                  variant="text"
+                  onClick={handleLogin}
+                  fullWidth
+                  sx={{
+                    color: "#E0E4EA",
+                    "&:hover": {
+                      backgroundColor: "transparent",
+                    },
+                  }}
+                >
+                  로그인
+                </Button>
+                <span className="text-white">|</span>
+                <Button
+                  variant="text"
+                  onClick={handleLogin}
+                  fullWidth
+                  sx={{
+                    color: "#E0E4EA",
+                    "&:hover": {
+                      backgroundColor: "transparent",
+                    },
+                  }}
+                >
+                  회원가입
+                </Button>
+              </div>
+            )}
+          </div>
+        )}
       </Drawer>
       <div className="grow-[1]">
         <Outlet />
